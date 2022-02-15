00c4c406804637db79e34cc638e5b8d4a0da7a8e
Adriano GAIBOTTI <adriano.gaibotti@gmail.com>
2022-02-15 13:56:55 +0100

    Completed sprintf tests example

--- tests/stdio/SprintfTest.cpp
+++ tests/stdio/SprintfTest.cpp
@@ -1,30 +1,43 @@
 extern "C"
 {
     #include <stdio.h>
+    #include <string.h>
 }
-
-// #includes for things with C++ linkage
 
 #include "CppUTest/TestHarness.h"
 
 TEST_GROUP(sprintf)
 {
-    // Define data accessible to test group members here.
+    char output[100];
+    const char *expected;
     void setup()
     {
-        // Initialization steps are executed before each TEST
+        memset(output, 0xaa, sizeof(output));
+        expected = "";
     }
     void teardown()
     {
-        // clean up steps are executed after each TEST
+    }
+    void expect(const char *s)
+    {
+        expected = s;
+    }
+    void given(int charsWritten)
+    {
+        LONGS_EQUAL(strlen(expected), charsWritten);
+        STRCMP_EQUAL(expected, output);
+        BYTES_EQUAL(0xaa, output[strlen(expected) + 1]);
     }
 };
 
-// Many test cases like this can be defined in the test file.
 TEST(sprintf, NoFormatOperations)
 {
-    char output[5] = "";
+    expect("hey");
+    given(sprintf(output, "hey"));
+}
 
-    LONGS_EQUAL(3, sprintf(output, "hey"));
-    STRCMP_EQUAL("hey", output);
+TEST(sprintf, InsertString)
+{
+    expect("Hello World\n");
+    given(sprintf(output, "Hello %s\n", "World"));
 }
